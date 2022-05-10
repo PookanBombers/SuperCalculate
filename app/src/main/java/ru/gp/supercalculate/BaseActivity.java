26f356e00b387cb94abcd55a0051242539f8d5cf
PookanBombers <tortellko@gmail.com>
2022-05-11 02:56:32 +0300

    этап 1

---
+++ app/src/main/java/ru/gp/supercalculate/BaseActivity.java
@@ -0,0 +1,52 @@
+package ru.gp.supercalculate;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    // Имя настроек
+    private static final String NameSharedPreference = "LOGIN";
+    // Имя параметра в настройках
+    private static final String themeSuperCalculate = "THEME_SUPER_CALCULATE";
+    protected static final int MyStandard = 0;
+    protected static final int MyOcean = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // Устанавливать тему надо только до установки макета активити
+        setTheme(getAppTheme(R.style.Standard_Theme));
+        super.onCreate(savedInstanceState);
+    }
+    protected int getAppTheme(int codeStyle) {
+        return codeStyleToStyleId(getCodeStyle(codeStyle));
+    }
+    // Чтение настроек, параметр «тема»
+    protected int getCodeStyle(int codeStyle){
+// Работаем через специальный класс сохранения и чтения настроек
+        SharedPreferences sharedPref = getSharedPreferences(NameSharedPreference,
+                MODE_PRIVATE);
+//Прочитать тему, если настройка не найдена - взять по умолчанию
+        return sharedPref.getInt(themeSuperCalculate, codeStyle);
+    }
+    // Сохранение настроек
+    protected void setAppTheme(int codeStyle) {
+        SharedPreferences sharedPref = getSharedPreferences(NameSharedPreference,
+                MODE_PRIVATE);
+// Настройки сохраняются посредством специального класса editor
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt(themeSuperCalculate, codeStyle);
+        editor.apply();
+    }
+    protected int codeStyleToStyleId(int codeStyle){
+        switch(codeStyle){
+            case MyOcean:
+                return R.style.Ocean_Theme;
+            default:
+                return R.style.Standard_Theme;
+        }
+    }
+
+}
--- app/src/main/java/ru/gp/supercalculate/MainActivity.java
+++ app/src/main/java/ru/gp/supercalculate/MainActivity.java
@@ -1,14 +1,56 @@
 package ru.gp.supercalculate;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.SharedPreferences;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioGroup;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.radiobutton.MaterialRadioButton;
+
+public class MainActivity extends BaseActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Animation1();
+        initThemeChooser();
+
     }
+
+    private void Animation1() {
+        ConstraintLayout constraintLayout = findViewById(R.id.mainLayout);
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2500);
+        animationDrawable.setExitFadeDuration(5000);
+        animationDrawable.start();
+    }
+    // Инициализация радиокнопок
+    private void initThemeChooser() {
+        initRadioButton(findViewById(R.id.standard),
+                MyStandard);
+        initRadioButton(findViewById(R.id.ocean),
+                MyOcean);
+        RadioGroup rg = findViewById(R.id.radioButtons);
+        ((MaterialRadioButton)rg.getChildAt(getCodeStyle(MyStandard))).setChecked(true);
+    }
+// Все инициализации кнопок очень похожи, поэтому создадим метод для переиспользования
+    private void initRadioButton(View button, final int codeStyle) {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+// сохраним настройки
+                setAppTheme(codeStyle);
+// пересоздадим активити, чтобы тема применилась
+                recreate();
+            }
+        });
+    }
+
 }
